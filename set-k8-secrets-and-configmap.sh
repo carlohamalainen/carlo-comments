4c4b48de96cceaf7d61e2129fd9583c75a1740d2
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2024-07-15 21:33:54 +1000

    secrets, config

--- set-k8-secrets-and-configmap.sh
+++ set-k8-secrets-and-configmap.sh
@@ -4,10 +4,21 @@ set -u
 set -e
 # set -x
 
+# The Promtail daemonset runs in the default namespace.
+
+NAMESPACE=default
+SECRETS=carlo-comments-secrets
+kubectl -n ${NAMESPACE} delete secret ${SECRETS} || echo "${SECRETS}"
+kubectl create secret generic ${SECRETS} \
+    --namespace=${NAMESPACE} \
+    --from-literal=LOKI_URL=${LOKI_URL} \
+    --from-literal=LOKI_USER=${LOKI_USER} \
+    --from-literal=LOKI_API_TOKEN=${LOKI_API_TOKEN}
+kubectl -n ${NAMESPACE} describe secret ${SECRETS}
+
+
 NAMESPACE=backend
-
 kubectl create namespace $NAMESPACE || echo $NAMESPACE
-
 SECRETS=carlo-comments-secrets
 kubectl -n ${NAMESPACE} delete secret ${SECRETS} || echo "${SECRETS}"
 kubectl create secret generic ${SECRETS} \
@@ -18,7 +29,8 @@ kubectl create secret generic ${SECRETS} \
     --from-literal=ADMIN_USER=${ADMIN_USER} \
     --from-literal=ADMIN_PASS=${ADMIN_PASS} \
     --from-literal=S3_REGION=${S3_REGION} \
-    --from-literal=S3_BUCKET=${S3_BUCKET}
+    --from-literal=S3_BUCKET=${S3_BUCKET} \
+    --from-literal=SES_IDENTITY=${SES_IDENTITY}
 kubectl -n ${NAMESPACE} describe secret ${SECRETS}
 
 REGISTRY=regcred
@@ -43,5 +55,5 @@ kubectl create configmap ${CONFIG_MAP} \
     --from-literal=LIMITER_RATE=${LIMITER_RATE} \
     --from-literal=LIMITER_BURST=${LIMITER_BURST} \
     --from-literal=COMMENT_HOST=${COMMENT_HOST} \
-    --from-literal=CORS_ALLOWED_ORIGINS="https://carlo-hamalainen.net,http://localhost,http://localhost:8000" # FIXME
+    --from-literal=CORS_ALLOWED_ORIGINS="https://carlo-hamalainen.net,http://localhost,http://localhost:8000,http://localhost:1313" # FIXME
 kubectl -n ${NAMESPACE} describe configmap ${CONFIG_MAP}
